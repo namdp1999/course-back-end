fa55424baae9b3bfaf1dc8ace9e3bb4bbd1fa01a
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-12-01 11:10:26 +0700

    commit message

---
+++ assets/tour/sql/categories.sql
@@ -0,0 +1,12 @@
+INSERT INTO categories (title, image, description, status, position, slug, deleted, deletedAt, createdAt, updatedAt)
+VALUES
+('Du lịch trong nước', 'https://backend.daca.vn/assets/tour/images/du-lich-trong-nuoc.jpg', 'Các tour du lịch trong Việt Nam', 'active', 1, 'du-lich-trong-nuoc', false, NULL, NOW(), NOW()),
+('Du lịch nước ngoài', 'https://backend.daca.vn/assets/tour/images/du-lich-nuoc-ngoai.jpg', 'Các tour du lịch quốc tế', 'active', 2, 'du-lich-nuoc-ngoai', false, NULL, NOW(), NOW()),
+('Tour mùa hè', 'https://backend.daca.vn/assets/tour/images/tour-mua-he.jpeg', 'Các tour phù hợp cho mùa hè', 'active', 3, 'tour-mua-he', false, NULL, NOW(), NOW()),
+('Tour mùa đông', 'https://backend.daca.vn/assets/tour/images/tour-mua-dong.jpg', 'Các tour phù hợp cho mùa đông', 'active', 4, 'tour-mua-dong', false, NULL, NOW(), NOW()),
+('Tour thám hiểm', 'https://backend.daca.vn/assets/tour/images/tour-tham-hiem.jpg', 'Các tour khám phá và thám hiểm', 'active', 5, 'tour-tham-hiem', false, NULL, NOW(), NOW()),
+('Tour nghỉ dưỡng', 'https://backend.daca.vn/assets/tour/images/tour-nghi-duong.jpg', 'Các tour nghỉ dưỡng tại các khu resort', 'active', 6, 'tour-nghi-duong', false, NULL, NOW(), NOW()),
+('Tour ẩm thực', 'https://backend.daca.vn/assets/tour/images/tour-am-thuc.jpg', 'Các tour trải nghiệm ẩm thực độc đáo', 'active', 7, 'tour-am-thuc', false, NULL, NOW(), NOW()),
+('Tour giáo dục', 'https://backend.daca.vn/assets/tour/images/tour-giao-duc.jpg', 'Các tour học thuật và giáo dục', 'active', 8, 'tour-giao-duc', false, NULL, NOW(), NOW()),
+('Tour thể thao', 'https://backend.daca.vn/assets/tour/images/tour-the-thao.jpg', 'Các tour liên quan đến hoạt động thể thao', 'active', 9, 'tour-the-thao', false, NULL, NOW(), NOW()),
+('Tour gia đình', 'https://backend.daca.vn/assets/tour/images/tour-gia-dinh.jpg', 'Các tour phù hợp cho cả gia đình', 'active', 10, 'tour-gia-dinh', false, NULL, NOW(), NOW());
---
+++ assets/tour/sql/tours_categories.sql
@@ -0,0 +1,12 @@
+INSERT INTO tours_categories (tour_id, category_id)
+VALUES
+(1, 1), -- Tour Hạ Long thuộc danh mục Du lịch trong nước
+(2, 1), -- Tour Đà Nẵng thuộc danh mục Du lịch trong nước
+(3, 1), -- Tour Nha Trang thuộc danh mục Du lịch trong nước
+(4, 1), -- Tour Sài Gòn thuộc danh mục Du lịch trong nước
+(5, 1), -- Tour Phú Quốc thuộc danh mục Du lịch trong nước
+(6, 1), -- Tour Đảo Ngọc Cô Tô thuộc danh mục Du lịch trong nước
+(7, 3), -- Tour Khám Phá Huế thuộc danh mục Tour mùa hè
+(8, 3), -- Tour Sapa thuộc danh mục Tour mùa hè
+(9, 3), -- Tour Vịnh Lan Hạ thuộc danh mục Tour mùa hè
+(10, 4); -- Tour Miền Tây thuộc danh mục Tour mùa đông
